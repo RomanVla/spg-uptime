1112a9b703b9f93ee5ab2bd028765263fb5bf0b7
Roman Vlasiuk <romanvl@interlink-ua.com>
2019-09-10 15:19:12 +0300

    Edited spg footer layout

--- inc/layout-footer-spg.php
+++ inc/layout-footer-spg.php
@@ -3,32 +3,31 @@
 
         <?php get_template_part('inc/content', 'footer-cta'); ?>
 
-        <div class="row justify-content-center mb-2">
-
-            <?php
-            if (is_active_sidebar('default_footer_3')) {
-                echo '<div class="col-12 py-2">';
-                dynamic_sidebar('default_footer_3');
-                echo '</div>';
-            }
-            ?>
-
-            <?php echo '<div class="clear"></div>'; ?>
-        </div>
-
         <div class="row mb-2">
             <?php
             if (is_active_sidebar('default_footer_1') && !(is_active_sidebar('default_footer_2'))) {
                 echo '<div class="col">';
-                dynamic_sidebar('default_footer_1');
+                    dynamic_sidebar('default_footer_1');
                 echo '</div>';
             }
-            if (is_active_sidebar('default_footer_2') && !(is_active_sidebar('default_footer_4'))) {
+            if (is_active_sidebar('default_footer_2') && !(is_active_sidebar('default_footer_3'))) {
                 echo '<div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2">';
-                dynamic_sidebar('default_footer_1');
+                    echo get_custom_logo();
+                    dynamic_sidebar('default_footer_1');
                 echo '</div><div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2">';
-                dynamic_sidebar('default_footer_2');
+                    dynamic_sidebar('default_footer_2');
                 echo '</div><div class="clear"></div>';
+            }
+            if (is_active_sidebar('default_footer_3')) {
+                echo '<div class="col-md-4 col-lg-4 col-sm-4 col-12 py-2">';
+                    echo get_custom_logo();
+                    dynamic_sidebar('default_footer_1');
+                echo '</div><div class="col-md-4 col-lg-4 col-sm-4 col-12 py-2">';
+                    dynamic_sidebar('default_footer_2');
+                echo '</div><div class="col-md-4 col-lg-4 col-sm-4 col-12 py-2">';
+                    dynamic_sidebar('default_footer_3');
+                echo '</div><div class="clear"></div>';
+
             }
             ?>
         </div>
