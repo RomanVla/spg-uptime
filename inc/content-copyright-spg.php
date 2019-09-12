3ae297bf28c0656da5b7a94a77eaf0b25cfbf4ed
Roman Vlasiuk <romanvl@interlink-ua.com>
2019-09-12 15:56:38 +0300

    [Footer] centered text

--- inc/content-copyright-spg.php
+++ inc/content-copyright-spg.php
@@ -4,7 +4,7 @@
 
 </div>
 
-<div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2">
+<div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2 text-center">
 
     <?php get_template_part('inc/content', 'copyright'); ?>
 
@@ -12,7 +12,7 @@
 
 <div class="col-md-3 col-lg-3 col-sm-3 col-12 py-2">
     <div class="d-flex justify-content-end mr-2">
-        <a class="text-small hover-arrow px-2" href="/privacy-policy/"> <small>Terms of service</small> </a>
-        <a class="text-small hover-arrow px-2" href="/privacy-policy/"> <small>Privacy Policy</small> </a>
+        <a class="text-small px-2" href="/privacy-policy/"> <small>Terms of service</small> </a>
+        <a class="text-small px-2" href="/privacy-policy/"> <small>Privacy Policy</small> </a>
     </div>
 </div>
