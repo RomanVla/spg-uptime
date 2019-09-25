aaa581fa6e2f337d411eeff48e24743982088ec5
Roman Vlasiuk <romanvl@interlink-ua.com>
2019-09-25 20:57:25 +0300

    Portfolio layout edited cards layout

--- loop/loop-portfolio-spg.php
+++ loop/loop-portfolio-spg.php
@@ -5,11 +5,17 @@
 		
 		<div id="portfolio-<?php the_ID(); ?>" <?php post_class( 'col-sm-6 col-lg-4 mb-4' ); ?> data-isotope-item data-category="<?php echo esc_attr( TommusRhodus_Framework()->the_terms( $post->ID, 'portfolio_category', 'name', ' ' ) ); ?>">
 			<a href="<?php the_permalink(); ?>">
-				<?php if( has_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail( 'large', array( 'class' => 'rounded is-shadow-box mb-3' ) ); ?>
-				<?php endif; ?>
-				<?php the_title( '<h4 class="mb-1">', '</h4>' ); ?>
-				<div class="text-small text-muted"><?php echo esc_attr( TommusRhodus_Framework()->the_terms( $post->ID, 'portfolio_category', 'name', ' ' ) ); ?></div>
+                    <div class="rounded is-shadow-box d-flex align-items-end" style='min-height: 500px; background-repeat: no-repeat; background-size: contain; background-image: url("<?php echo get_the_post_thumbnail_url() ?>")'>
+                        <div>
+				        <div class="text-small text-muted px-4 bg-light"><?php echo esc_attr( TommusRhodus_Framework()->the_terms( $post->ID, 'portfolio_category', 'name', ' ' ) ); ?></div>
+                        <div class="px-2 bg-secondary"><?php the_title( '<h4 class="mb-0">', '</h4>' ); ?></div>
+                        </div>
+                    </div>
+                <?php
+                    if( has_excerpt() ){
+                    echo '<div class="text-small text-dark p-1">'. get_the_excerpt() .'</div>';
+                    }
+                 ?>
 			</a>
 		</div>
 			
