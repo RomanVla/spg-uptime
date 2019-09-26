d19aaccad747b5313300fa06c686123bce19b873
Roman Vlasiuk <romanvl@interlink-ua.com>
2019-09-26 17:11:56 +0300

    Portfolio layout edited portfolio filter style

--- loop/loop-portfolio-spg.php
+++ loop/loop-portfolio-spg.php
@@ -7,7 +7,7 @@
 			<a href="<?php the_permalink(); ?>">
                     <div class="rounded is-shadow-box d-flex align-items-end project-card" style= 'background-image: url("<?php echo get_the_post_thumbnail_url() ?>")'>
                         <div class="project-card-label">
-				            <div class="text-small text-muted px-4 project-card-label__term"><?php echo esc_attr( TommusRhodus_Framework()->the_terms( $post->ID, 'portfolio_category', 'name', ' ' ) ); ?></div>
+				            <div class="text-small text-muted px-4 project-card-label__term"><?php echo esc_attr( TommusRhodus_Framework()->the_terms( $post->ID, 'portfolio_category', 'name', ' | ', false, true ) ); ?></div>
                             <div class="p-2 text-light project-card-label__title">
                                 <?php the_title( '<h4 class="mb-0">', '</h4>' ); ?>
                             </div>
