403e25c854cc3b791bdb4732a52893880f00a585
Roman Vlasiuk <romanvl@interlink-ua.com>
2019-10-07 15:29:21 +0300

    Changed align footer in mobile

--- inc/layout-footer-spg.php
+++ inc/layout-footer-spg.php
@@ -11,22 +11,22 @@
                 echo '</div>';
             }
             if (is_active_sidebar('default_footer_2') && !(is_active_sidebar('default_footer_3'))) {
-                echo '<div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2">';
+                echo '<div class="col-md-6 col-lg-6 col-sm-12 col-12 py-2">';
                     echo '<div class="widget">';
                         echo get_custom_logo('');
                     echo '</div>';
                     dynamic_sidebar('default_footer_1');
-                echo '</div><div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2">';
+                echo '</div><div class="col-md-6 col-lg-6 col-sm-12 col-12 py-2">';
                     dynamic_sidebar('default_footer_2');
                 echo '</div><div class="clear"></div>';
             }
             if (is_active_sidebar('default_footer_3')) {
-                echo '<div class="col-md-3 col-lg-3 col-sm-3 col-12 py-2">';
+                echo '<div class="col-md-3 col-lg-3 col-sm-12 col-12 py-2">';
                     echo get_custom_logo();
                     dynamic_sidebar('default_footer_1');
-                echo '</div><div class="col-md-6 col-lg-6 col-sm-6 col-12 py-2">';
+                echo '</div><div class="col-md-6 col-lg-6 col-sm-12 col-12 py-2">';
                     dynamic_sidebar('default_footer_2');
-                echo '</div><div class="col-md-3 col-lg-3 col-sm-3 col-12 py-2">';
+                echo '</div><div class="col-md-3 col-lg-3 col-sm-12 col-12 py-2">';
                     dynamic_sidebar('default_footer_3');
                 echo '</div><div class="clear"></div>';
 
